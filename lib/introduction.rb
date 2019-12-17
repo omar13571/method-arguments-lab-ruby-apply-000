69c37f7cdd5dd38696aa8aa9d134f60d338ac3e3
peaceful-distribution-8695 <porcaoma.1357@gmail.com>
2019-12-17 08:09:53 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,6 +2,6 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-def introduction_with_language(name_language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+def introduction_with_language(name,language)
+  puts "Hi, my name is #{name} and I am learning to programe in #{language}."
 end
